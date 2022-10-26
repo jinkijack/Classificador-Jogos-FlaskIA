f6909911daf88c14bc00fc084f2cf8b28ca797d9
Tiago Mateus Carvalho <tiagomc123@hotmail.com>
2022-10-25 21:13:53 -0300

    index changes and select working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 from flask import Flask, jsonify, request, render_template
-
+import math
 import pickle
 
 app = Flask(__name__)
@@ -18,8 +18,8 @@ def predict():
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
     pred = model.predict(final_features)
-
-    return render_template("index.html", prediction_text="Iris " + str(pred[0]))
+    x = pred[0]
+    return render_template("index.html", prediction_text="Provável valor de vendas globais em milhões: " + str(format(x, '.2f')))
 
 
 @app.route("/api", methods=["POST"])
